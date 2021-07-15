b659d496adfba720d22734c919dbf390bee97e1c
SPZCNSK <sapiezcinski.juan@gmail.com>
2021-07-15 10:47:01 -0300

    adição do nome listen personalizado no cabeçalho

--- ExecutaPrograma.java
+++ ExecutaPrograma.java
@@ -21,9 +21,18 @@ public class ExecutaPrograma {
 		 	ConfirPagam confirmacaopag = new ConfirPagam();
 		 	
 		    int tecla = 0;
-           System.out.println("===================================================================================");
-           System.out.println("                           *-* Bem vindes à Listen *-*                             ");
-           System.out.println("===================================================================================");
+	           System.out.println("===================================================================================");
+	           System.out.println("                ,gggg,                                                             ");
+	           System.out.println("               d8'  '8I                    I8                                      ");
+	           System.out.println("               88  ,dP                     I8                                      ");
+	           System.out.println("             8888888P'     gg            88888888                                  ");
+	           System.out.println("               88         ''               I8                                      ");  
+	           System.out.println("               88         gg     ,g,       I8     ,ggg,    ,ggg,,ggg,              ");
+	           System.out.println("          ,aa,_88         88    ,8'8,      I8    i8' '8i  ,8' ''8P' '8,            ");
+	           System.out.println("         dP' '88P         88   ,8'  Yb    ,I8,   I8, ,8I  I8    8I   8I            ");
+	           System.out.println("         Yb,_,d88b,,_   _,88,_,8'_   8)  ,d88b,  `YbadP' ,dP   8I    Yb,           ");
+	           System.out.println("          'Y8P'  'Y888888P''Y8P' 'YY8P8P 8P''Y8 888P'Y8888P'   8I   `Y8            ");
+	           System.out.println("===================================================================================");
            String menu = "\n\n<> <> <> <> <> <> <>     Não existe saúde, sem saúde mental     <> <> <> <> <> <> <>\n\n1 - Atendimento Online - Psicologia\n2 - Sair\nItem:";
 
            do {
